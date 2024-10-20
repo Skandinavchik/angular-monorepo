4e39dfaae9626b306a0e109a6850b9a92a90b1e6
Artem Sokolov <sknd2569@gmail.com>
2024-10-20 11:40:43 +0200

    added user-card component

--- apps/easy-tasks/src/app/app.component.ts
+++ apps/easy-tasks/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { UserCardComponent } from './users/user-card/user-card.component'
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, UserCardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
